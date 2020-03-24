ed9ca7b22c2e1b1c554dde1afdb9ad8d3731b251
venkatesh <venkatesh.bheemagani@gmail.com>
2020-03-23 19:18:13 -0700

    11

--- src/test/java/io/junit5/MathUtilsTest.java
+++ src/test/java/io/junit5/MathUtilsTest.java
@@ -8,7 +8,17 @@ class MathUtilsTest {
 
 	@Test
 	void test() {
-      System.out.println("this is a test ran");
+      MathUtils mathUtils = new  MathUtils();
+      int expected = 3;
+     int actual=  mathUtils.add(1, 2);
+      assertEquals(expected, actual,"add method");
 	}
-
+    
+	
+	@Test
+	void testComputeCircleRadius()
+	{
+		MathUtils mathUtils = new MathUtils();
+		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10),"should return right answers");
+	}
 }
