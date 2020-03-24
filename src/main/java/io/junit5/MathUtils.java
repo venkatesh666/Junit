613b605b5fcfc60dce0b43cb1b94397aa9e7127b
venkatesh <venkatesh.bheemagani@gmail.com>
2020-03-23 18:07:35 -0700

    junit5

---
+++ src/main/java/io/junit5/MathUtils.java
@@ -0,0 +1,8 @@
+package io.junit5;
+
+public class MathUtils {
+	
+	public int add(int a, int b) {
+		return a+b;
+	}
+}
---
+++ src/test/java/io/junit5/MathUtilsTest.java
@@ -0,0 +1,14 @@
+package io.junit5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void test() {
+      System.out.println("this is a test ran");
+	}
+
+}
